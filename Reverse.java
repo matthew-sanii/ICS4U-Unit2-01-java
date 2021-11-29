c440ef2f4d09a3f25fa4f7307e174a2a68728be6
Ubuntu <ubuntu@ip-172-31-89-75.ec2.internal>
2021-11-29 21:13:44 +0000

    edited Reverse.java

--- Reverse.java
+++ Reverse.java
@@ -9,26 +9,54 @@
 
 import java.util.Scanner;
 
+/**
+* This is the reverse program.
+*/
+
 final class Reverse {
 
+    /**
+    * Prevent instantiation
+    * Throw an exception IllegalStateException.
+    * if this ever is called
+    *
+    * @throws IllegalStateException
+    *
+    */
     private Reverse() {
         throw new IllegalStateException("Cannot be instantiated");
     }
 
+    /**
+    * Randomly chooses what the computer will use in Rock Paper Scissors.
+    *
+    * @param str the user's input
+    * @return the reversed string
+    */
+
     public static String backWords(String str) {
+        final String result;
         if (str.isEmpty()) {
-            return str;
-        } else {
-            return backWords(str.substring(1)) + str.charAt(0);
+            result = "";
         }
+        else {
+            result = backWords(str.substring(1)) + str.charAt(0);
+        }
+        return result;
     }
 
+    /**
+    * The starting main() function.
+    *
+    * @param args Name of file containing a string of numbers
+    */
+
     public static void main(String[] args) {
-        Scanner scanner = new Scanner(System.in);
-        String str;
+        final Scanner scanner = new Scanner(System.in);
+        final String str;
         System.out.println("Enter what you want reversed: ");
         str = scanner.nextLine();
-        String reversed = backWords(str);
+        final String reversed = backWords(str);
         System.out.println("The reversed string is: " + reversed + ".");
     }
 }
